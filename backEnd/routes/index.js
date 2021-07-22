bd07787550dd95026d0a045174204ccb92001dc3
Timothée Perakis-Valat <tperakis@wharton.upenn.edu>
2021-07-22 15:28:18 +0200

    base de donnée connectée

--- backEnd/routes/index.js
+++ backEnd/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var eventsModel = require('../models/events')
+
 
 
 /* GET home page. */
@@ -8,8 +10,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/add-event', function(req, res, next){
-  
+router.post('/add-event', async function(req, res, next){
+  var newEvent = new eventsModel({
+    adminID: req.body.adminID,
+    private: req.body.private,
+    title: req.body.title,
+    desc: req.body.desc,
+    image: req.body.image,
+    address: req.body.address,
+    longitude: req.body.longitude,
+    latitude: req.body.latitude,
+    date: req.body.date,
+    tags: req.body.tags
+    })
+
+    var event = await newEvent.save();
+    
+    res.json({event})
+
 })
 
 module.exports = router;
