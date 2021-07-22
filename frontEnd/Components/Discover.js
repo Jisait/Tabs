dbc6b0b18f6b0e6a71aa2a75cb35095e7d450d81
JeanCharles Cousin <cousin.jeanch@gmail.com>
2021-07-22 14:54:48 +0200

    ajout avatar Discover

--- frontEnd/Components/Discover.js
+++ frontEnd/Components/Discover.js
@@ -1,6 +1,6 @@
 import React, { useState }  from 'react';
 import AppLoading from 'expo-app-loading';
-import { ImageBackground, Text, View,  StyleSheet, Dimensions, ScrollView } from 'react-native';
+import { Image, ImageBackground, Text, View,  StyleSheet, Dimensions, ScrollView } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons'; 
 import { FontAwesome5 } from '@expo/vector-icons'; 
 import { LinearGradient } from 'expo-linear-gradient';
@@ -75,7 +75,7 @@ let changeLikeColor = () => {
     return (
       <View style={{flex:1, alignItems: 'center'}}>
 
-<ScrollView style={{flex:1}} snapToInterval={(6.5/10)*screen.height} decelerationRate='fast'> git 
+<ScrollView style={{flex:1}} snapToInterval={(6.5/10)*screen.height} decelerationRate='fast'> 
       
       
       <View style={{flex: 1, height: (6.5/10)*screen.height, flexDirection: 'column', width: (9/10)*screen.width, paddingTop: 30}}>
@@ -125,57 +125,16 @@ let changeLikeColor = () => {
           <View style={{width: '100%',position: 'relative', left: 36, bottom: 0}}>
               <Text style={styles.adresse}>101 boulevard Voltaire, 75011 Paris</Text>
           </View>
+          <View style={{alignItems: 'center', flexDirection: 'row', position: 'absolute', left: 20, top: 45}}>
+              <ImageBackground source={require("../assets/avatar_fake.jpg")} imageStyle={{ border: 'white', borderRadius: 50}} style={ styles.imgAvatar}/>
+              <Text style={styles.pseudo}>Cool_Girl_75</Text>
+           
+            
+          </View>
     
         </View>
         
-        <View style={{flex: 1, height: (6.5/10)*screen.height, flexDirection: 'column', width: (9/10)*screen.width, paddingTop: 30}}>
-      
-  
-           
-      <ImageBackground position= 'relative' source={require("../assets/dance.jpg")} imageStyle={{ borderRadius: 28, marginBottom: 25}} style={ styles.imgBackground }>
-             <LinearGradient
-                 colors={['transparent','rgba(0,0,0,0.8)']}
-                 style={{
-                   position: 'absolute',
-                   left: 0,
-                   right: 0,
-                   
-                   bottom:0,
-                   height: '50%',
-                   borderRadius: 28
-                 }}/>
-         </ImageBackground>
-
-         <View style={{position: 'absolute', right: 20, top: 45}}>
-           <FontAwesome onPress={() => changeLikeColor()} name="heart" size={30} color={likeColor} />
-         </View>
-
-         <View style={{position: 'absolute', left: 150, bottom: 166}}>
-           <FontAwesome5 name="map-marker-alt" size={26} color="white" />
-         </View>
-
-         <View style={{position: 'absolute', left: 190, bottom: 166}}>
-           <FontAwesome name="share-alt" size={26} color="white" />
-         </View>
-      
-
-         <View style={{width: '85%', position: 'absolute', left: 36, bottom: 114}}>
-             <Text style={styles.text}>DANCE SHOW</Text>
-         </View>
-         <View style={{width: '85%',position: 'absolute', left: 36, top: (0.78)*(6.5/10)*screen.height}}>
-             <Text style={styles.subtext}>En cela, le spectacle vivant désigne de nombreux modes d'expression artistique.</Text>
-         </View>
-         <View style={{width: '100%',position: 'absolute', left: 28, bottom: 32}}>
-           <View style={styles.hairlineWhite} />
-         </View>
-         <View style={{width: '100%',position: 'absolute', left: 28, bottom: 6}}>
-             <View style={styles.hairlineBlack} />
-         </View> 
-         <View style={{width: '100%',position: 'relative', left: 36, bottom: 0}}>
-             <Text style={styles.adresse}>101 boulevard Voltaire, 75011 Paris</Text>
-         </View>
-   
-       </View>
+     
 
         
 
@@ -239,12 +198,24 @@ imgBackground: {
     flex: 1 ,
     },
 
+imgAvatar: {
+    width: 45,
+    height: 45,
+},
+
 imgBackground2: {
     position: 'absolute',
     width: '100%',
     height: '70%',
-    flex: 1 ,
+    flex: 1
     },
+
+    pseudo: {
+      color: 'white',
+      marginLeft: 8,
+      fontFamily: 'Poppins_500Medium',
+
+    }
 
 });
 
